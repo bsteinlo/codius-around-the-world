08c57e7cf5b3c60770696c5708957578df063b82
Torrie Fischer <torrie@ripple.com>
2014-11-18 16:01:51 -0800

    refactor and implement updating of all dynamic text

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,25 @@
-function getTotal() {
-	jQuery.ajax({
-	  type: "POST",
-	  url: 'https://api.ripplecharts.com/api/total_network_value',
-	  data:{
-		exchange  : {
-			currency  : "USD",         
-			issuer    : "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
+function doUpdate(element, endpoint) {
+	$.ajax({
+		type: "GET",
+		url: 'https://codiuscheckin.localtunnel.me/'+endpoint,
+		success: function(data) {
+			element.html(data);
 		}
-	},
-	  success: function(data) {
-			jQuery('.total_num').html('$' + parseInt(data.total));
-			console.log(data);
-	  },
-	  dataType: 'json'
 	});
 }
 
-window.setInterval(function(){
-  getTotal();
-  jQuery('.total_num').fadeOut('slow');
-  jQuery('.total_num').fadeIn('slow');
-}, 5000);
+$(document).ready(function() {
+	updateAll();
+});
+
+function updateAll() {
+	doUpdate($('.total_num'), 'tobedonated');
+	doUpdate($('.charity0'), 'charity0');
+	doUpdate($('.charity1'), 'charity1');
+	doUpdate($('.charity2'), 'charity2');
+	doUpdate($('.donate_name'), 'name');
+	doUpdate($('.phonenumber0'), 'phonenumber0');
+	doUpdate($('.phonenumber1'), 'phonenumber1');
+	doUpdate($('.phonenumber2'), 'phonenumber2');
+	window.setTimeout(updateAll, 1000);
+};
